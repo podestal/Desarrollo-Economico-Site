c442f738fbab6cf96cdfb3a4e512cc4f7186813a
podestal <luisrodriguezpodesta@gmail.com>
2024-07-04 09:38:59 -0500

    effects to emergency section

--- src/components/landing-page/Emergency.tsx
+++ src/components/landing-page/Emergency.tsx
@@ -1,13 +1,23 @@
 import { RiHome3Fill, RiPhoneFill } from "@remixicon/react"
 import { Icon } from "@tremor/react"
+import {motion} from 'framer-motion'
 
 const Emergency = () => {
   return (
     <section id="emergency" className="w-[100%] bg-white min-h-screen flex flex-col justify-start items-center gap-12 p-12 overflow-hidden">
-        <h2 className="text-7xl lg:text-8xl font-montserrat text-sate-900 w-full text-center">Servicios de Emergencia</h2>
+        <motion.h2 
+            initial={{opacity: 0, translateY: 100}}
+            whileInView={{opacity: 1, translateY: 0}}
+            transition={{duration: 2}}
+            className="text-7xl lg:text-8xl font-montserrat text-sate-900 w-full text-center">Servicios de Emergencia</motion.h2>
         <div className={`w-[85%] grid grid-cols-1 xl:grid-cols-3 gap-20 text-slate-900`}>
-            <div className="card-wrapper">
-                <div className="card-content py-8 px-4 flex flex-col justify-center items-center gap-6 rounded-3xl">
+            <motion.div 
+                initial={{opacity: 0, translateX: 200}}
+                whileInView={{opacity: 1, translateX: 0}}
+                transition={{duration: 2}}
+                className="card-wrapper">
+                <div 
+                    className="card-content py-8 px-4 flex flex-col justify-center items-center gap-6 rounded-3xl">
                     <h3 className="text-6xl font-palanquin mb-6  font-bold">Policía</h3>
                     <div className="w-full flex justify-start items-center gap-6">
                         <Icon icon={RiHome3Fill} size="xl"/>
@@ -18,8 +28,12 @@ const Emergency = () => {
                         <p>054-532558</p>
                     </div>
                 </div>  
-            </div>
-            <div className=" py-8 px-4 flex flex-col justify-center items-center gap-6 rounded-3xl  shadow-slate-950 shadow-2xl">
+            </motion.div>
+            <motion.div 
+                initial={{opacity: 0, translateX: -200}}
+                whileInView={{opacity: 1, translateX: 0}}
+                transition={{duration: 2}}
+                className=" py-8 px-4 flex flex-col justify-center items-center gap-6 rounded-3xl  shadow-slate-950 shadow-2xl">
                 <h3 className="text-6xl font-palanquin mb-6  font-bold">Hospital</h3>
                 <div className="w-full flex justify-start items-center gap-6">
                     <Icon icon={RiHome3Fill} size="xl"/>
@@ -29,8 +43,12 @@ const Emergency = () => {
                     <Icon icon={RiPhoneFill} size="xl" color="blue"/>
                     <p>054-532558</p>
                 </div>
-            </div>
-            <div className="card-wrapper">
+            </motion.div>
+            <motion.div 
+                initial={{opacity: 0, translateX: 200}}
+                whileInView={{opacity: 1, translateX: 0}}
+                transition={{duration: 2.4}}
+                className="card-wrapper">
                 <div className="card-content py-8 px-4 flex flex-col justify-center items-center gap-6 rounded-3xl  ">
                     <h3 className="text-6xl font-palanquin mb-6  font-bold">Serenazgo</h3>
                     <div className="w-full flex justify-start items-center gap-6">
@@ -42,7 +60,7 @@ const Emergency = () => {
                         <p>054-532558</p>
                     </div>
                 </div>
-            </div>
+            </motion.div>
         </div>
     </section>
   )
